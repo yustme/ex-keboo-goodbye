e71a481f00338f1a4477cb8e764d87a5bf752c71
Vojta Tůma <vojta@usetreno.cz>
2018-11-01 12:37:08 +0100

    changed main script

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+import csv
 
-print("Hello world from python")
+print("Hello, World! from python")
+
+csvlt = '\n'
+csvdel = ','
+csvquo = '"'
+with open('/data/in/tables/source.csv', mode='rt', encoding='utf-8') as in_file:
+    lazy_lines = (line.replace('\0', '') for line in in_file)
+    reader = csv.DictReader(lazy_lines, lineterminator=csvlt, delimiter=csvdel, quotechar=csvquo)
+    for row in reader:
+        # do something
+        print("The first row is ", row)
+        # we don't want to print the entire file to the output
+        exit()
