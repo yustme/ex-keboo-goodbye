e6430bf9cf057a15f731ed1ae4140f1245041fc0
Vojta Tůma <vojta@usetreno.cz>
2018-11-01 13:09:45 +0100

    testing configuration file

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import csv
+# Load the KBC library to process the config file
+from keboola import docker
+cfg = docker.Config('/data/')
+params = cfg.get_parameters()
 
-print("Hello universe from Vojta")
+print("Hello world from python")
 
 csvlt = '\n'
 csvdel = ','
@@ -12,19 +16,22 @@ with open('/data/in/tables/source.csv', mode='rt', encoding='utf-8') as in_file,
     reader = csv.DictReader(lazy_lines, lineterminator=csvlt, delimiter=csvdel,
                             quotechar=csvquo)
 
-    even_writer = csv.DictWriter(odd_file, fieldnames=reader.fieldnames,
-                                 lineterminator=csvlt, delimiter=csvdel,
-                                 quotechar=csvquo)
-    even_writer.writeheader()
-
-    odd_writer = csv.DictWriter(even_file, fieldnames=reader.fieldnames,
+    odd_writer = csv.DictWriter(odd_file, fieldnames=reader.fieldnames,
                                 lineterminator=csvlt, delimiter=csvdel,
                                 quotechar=csvquo)
     odd_writer.writeheader()
+
+    even_writer = csv.DictWriter(even_file, fieldnames=reader.fieldnames,
+                                 lineterminator=csvlt, delimiter=csvdel,
+                                 quotechar=csvquo)
+    even_writer.writeheader()
     i = 0
     for row in reader:
         if i % 2 == 0:
             even_writer.writerow(row)
         else:
-            odd_writer.writerow(row)
+            newRow = {}
+            for key in reader.fieldnames:
+                newRow[key] = row[key] + ''.join([params['sound']] * params['repeat'])
+            odd_writer.writerow(newRow)
         i = i + 1
